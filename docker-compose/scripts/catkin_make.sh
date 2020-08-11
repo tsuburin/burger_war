59aa7e370e38306700a9e9af35a5a677ca159e2a
tsuburin <tsuburin@gmail.com>
2020-08-11 23:30:00 +0900

    開発環境と実行環境を分離

--- docker-compose/scripts/catkin_make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-script_dir=$(cd $(dirname $0) && pwd)
-compose_dir=$(cd $script_dir/.. && pwd)
-
-cd $compose_dir
-docker-compose exec -w "/home/burger/catkin_ws" burger bash -i -c 'exec catkin_make "$@"' "catkin_make" "$@"
